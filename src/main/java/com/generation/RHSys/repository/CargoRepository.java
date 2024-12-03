14a2cc754f1962a6dd048f65dd1260492116812e
Martinelii <Kevenmartineli@outlook.com>
2024-12-03 11:17:13 -0300

    :sparkles: Controller e Repository Cargo

--- src/main/java/com/generation/RHSys/repository/CargoRepository.java
+++ src/main/java/com/generation/RHSys/repository/CargoRepository.java
@@ -1,5 +1,13 @@
 package com.generation.RHSys.repository;
 
-public interface CargoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.RHSys.model.Cargo;
+
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+
+	public List <Cargo> findByTituloContainingIgnoreCase(@Param("cargo") String cargo); 
 }
