4e274fad7e0e42f1af391a9b3ecfc449c0a84b9f
Kagradiel <matheusp_torres@hotmail.com>
2024-12-03 09:53:33 -0300

    Primeiro commit

---
+++ src/main/java/com/generation/RHSys/RhSysApplication.java
@@ -0,0 +1,13 @@
+package com.generation.RHSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhSysApplication.class, args);
+	}
+
+}
